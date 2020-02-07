5da7efc3f349a9028e0fa18109bf5403327fe954
Soul1 <darkwaud@gmail.com>
2020-02-08 00:35:55 +0300

    added Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,13 +5,19 @@ import css from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className={css.footer}>
             <WorkSchedule/>
+            <h3 className={css.name}>
+                Мидаев Ахмад
+            </h3>
             <div className={css.social}>
-            <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='https://yandex.ru/'/>
-            <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='https://yandex.ru/'/>
-            <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='https://yandex.ru/'/>
-        </div>
+                <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='/contact'/>
+                <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='/contact'/>
+                <Social src='https://s3-cdn.mogul.gg/player/anh_lee.jpg?t=1550325834.39417' link='/contact'/>
+            </div>
+            <div className={css.copy}>
+                2020
+            </div>
         </div>
     )
 }
