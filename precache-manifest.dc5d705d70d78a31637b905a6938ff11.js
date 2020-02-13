0f3ddf2473638efa065c4c9f7e50ddd613b848d8
Soul1 <darkwaud@gmail.com>
2020-02-13 21:49:28 +0300

    Updates

---
+++ precache-manifest.dc5d705d70d78a31637b905a6938ff11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f8322d0902c3f0394e6abcc7b8adce",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "becd98838fd4f582dc80",
+    "url": "/Portfolio/static/css/main.aa5508e2.chunk.css"
+  },
+  {
+    "revision": "82ce70e8cfac13da22df",
+    "url": "/Portfolio/static/js/2.d97e3805.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Portfolio/static/js/2.d97e3805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "becd98838fd4f582dc80",
+    "url": "/Portfolio/static/js/main.57c38d57.chunk.js"
+  },
+  {
+    "revision": "076423558f3d7aa322df",
+    "url": "/Portfolio/static/js/runtime-main.ec71b170.js"
+  }
+]);
